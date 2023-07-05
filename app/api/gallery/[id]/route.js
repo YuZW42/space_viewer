e24d1c04da7a6bad78d0d71b8c6db36d643acf0c
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2023-07-05 16:05:10 -0400

    starting nasa search API capability

---
+++ app/api/gallery/[id]/route.js
@@ -0,0 +1,11 @@
+export const GET = async (request, { params }) => {
+    try {
+        const res = await fetch("https://images-api.nasa.gov/search?q=" + params.id)
+        data = await res.json()
+        console.log(data)
+
+        return new Response(JSON.stringify(data), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch NASA API images", { status: 500 })
+    }
+}
