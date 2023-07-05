729f9c0f24533dffc91a5468d9e52d786dd07830
William Zeng <88296782+YuZW42@users.noreply.github.com>
2023-07-04 17:13:53 -0700

    finished view of the day

--- app/api/daily/route.js
+++ app/api/daily/route.js
@@ -1,8 +1,9 @@
 // Fetches the location data from the NASA API
-async function getData() {
+async function getData(date) {
     try{
-        const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}`);
+        const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}&&date=${date}`, { cache: 'no-store' });
         const data = await res.json();
+        console.log('show',data)
         return data;
     } catch (error) {
         return error;
@@ -23,8 +24,12 @@ function formatData(data) {
 // Export data from API endpoint
 export const GET = async (request) => {
     try{
-        const data = await getData();
+        const{searchParams} = new URL(request.url);
+        const date = searchParams.get('date');
+        
+        const data = await getData(date);
         const formattedData = formatData(data);
+        console.log('hey',formattedData);
         return new Response(JSON.stringify(formattedData), {status: 200})
     } catch (error) {
         return new Response(error, {status: 500})
--- app/daily/page.jsx
+++ app/daily/page.jsx
@@ -1,38 +1,60 @@
 "use client";
-
 import { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { format, parseISO } from 'date-fns';
 
 const Daily = () => {
-    const [dailyData, setDailyData] = useState([]);
+  const [dailyData, setDailyData] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
 
-    const fetchDailyPic = async () => {
-        
-        const response = await fetch('/api/daily');
-        const  responseData = await response.json();
+  const fetchDailyPic = async (date) => {
+    const response = await fetch(`/api/daily?date=${date}`);
+    const responseData = await response.json();
+    setDailyData(responseData);
+  };
 
-        setDailyData(responseData);
+  const formatDateFn = (date) => {
+    if (date) {
+      const formattedDate = format(date, 'yyyy-MM-dd');
+      return formattedDate;
     }
+    return '';
+  };
 
-    useEffect(() => {
-        fetchDailyPic();
-    }, []);
+  useEffect(() => {
+    fetchDailyPic(selectedDate);
+  }, [selectedDate]);
+  useEffect(() => {
+    fetchDailyPic('');
+  }, []);
 
+  // Render the location data
+  return (
+    <div>
+      <DatePicker
+        selected={selectedDate ? parseISO(selectedDate) : null}
+        onChange={(date) => setSelectedDate(formatDateFn(date))}
+        dateFormat="yyyy-MM-dd"
+        maxDate={new Date()}
+      />
 
-    // Render the location data
-    return (
+      {dailyData ? (
         <div>
-            {dailyData ? (
-                <div>
-                    <div>Welcome to the Picture of the Day</div>
-                    <h4>{dailyData.date}</h4>
-                    <h4>{dailyData.explanation}</h4>
-                    <img src={dailyData.hdurl} alt="NASA Picture of the Day" style={{ width: 800 }} />
-                </div>
-            ) : (
-                <div>Loading...</div>
-            )}
+          <div>Welcome to the Picture of the Day</div>
+          <h4>{dailyData.date}</h4>
+          <h4>{dailyData.explanation}</h4>
+          <img
+            src={dailyData.hdurl}
+            alt="NASA Picture of the Day"
+            style={{ width: 800 }}
+          />
         </div>
-    )
-}
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+};
 
-export default Daily
+export default Daily;
